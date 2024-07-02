c8d8ee211c544338443043f6609f2b8a398a1f12
Ryzel <ryzelerinfelizco@gmail.com>
2024-07-02 18:10:37 +0800

    Updated Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutMeController;
+use App\Http\Controllers\SkillsController;
+use App\Http\Controllers\HobbiesController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/about-me', [AboutMeController::class, 'index'])->name('about-me');
+Route::get('/skills', [SkillsController::class, 'index'])->name('skills');
+Route::get('/hobbies', [HobbiesController::class, 'index'])->name('hobbies');
