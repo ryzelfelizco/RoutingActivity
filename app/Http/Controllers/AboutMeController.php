689e10ac67e0b795e158fd2b74ebf2d98360dcea
Ryzel <ryzelerinfelizco@gmail.com>
2024-07-02 18:19:54 +0800

    Updated About Me Controller

--- app/Http/Controllers/AboutMeController.php
+++ app/Http/Controllers/AboutMeController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class AboutMeController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = ['name' => 'Ryzel Erin Felizco', 'age' => '21', 'birthdate' => 'August 08, 2002', 'Location' => 'Baguio City'];
+        return view('about-me', $data);
+    }
 }
